89dbfa7a2f802c2fd8f5b41a44eef510c2eba50f
Russell Davidson <russelldavidson10@gmail.com>
2025-03-30 02:34:26 -0400

    Added TextObject Prefab

--- src/RealmClient/Assets/_Scripts/TextObject.cs
+++ src/RealmClient/Assets/_Scripts/TextObject.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 namespace Realm
 {
@@ -7,43 +11,104 @@ namespace Realm
     {
         private Vector3 originalRotation;
 
-        public GameObject border;
+        public GameObject textObjectCollider;
 
-        public Camera mainCamera;
+        public TextMeshPro textMeshProObject;
+
+        public GameObject textObjectUI;
+
+        public Button saveButton;
+
+        public TMP_InputField inputField;
+
+        public Camera arCamera;
+
+        public ARRaycastManager arRaycastManager;
+
+        private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        private TMP_Text boardText;
 
         void Awake()
         {
             originalRotation = transform.rotation.eulerAngles;
         }
 
+        void OnEnable()
+        {
+            saveButton.onClick.AddListener(Save);
+            boardText = textMeshProObject.GetComponent<TMP_Text>();
+            inputField.onValueChanged.AddListener(UpdateBoard);
+            // TODO: Get from DB and if not default text
+            inputField.text = "HELLO THERE";
+        }
+
         void Update()
         {
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
+            if (textObjectUI.activeSelf)
+                return;
 
-                if (touch.phase == TouchPhase.Began)
+            // if (Input.touchCount > 0)
+            // {
+            //     Touch touch = Input.GetTouch(0);
+
+            //     if (touch.phase == TouchPhase.Began)
+            //     {
+            //         Debug.Log("touch");
+            //         Ray ray = arCamera.ScreenPointToRay(touch.position);
+            //         if (Physics.Raycast(ray, out RaycastHit hitObject))
+            //         {
+            //             Debug.Log(hitObject.transform.gameObject);
+            //             if (hitObject.transform.gameObject == textObjectCollider)
+            //             {
+            //                 Debug.Log("HIT!");
+            //             }
+            //         }
+            //     }
+            // }
+
+            if (Input.GetMouseButton(0))
+            {
+                Ray ray = arCamera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hitObject))
                 {
-                    Ray ray = mainCamera.ScreenPointToRay(touch.position);
-                    RaycastHit raycastHit;
-                    if (Physics.Raycast(ray, out raycastHit))
+                    Debug.Log(hitObject.transform.gameObject);
+                }
+
+                if (arRaycastManager.Raycast(ray, hits, TrackableType.AllTypes))
+                {
+                    if (hitObject.transform.gameObject == textObjectCollider)
                     {
-                        Debug.Log(raycastHit.transform.gameObject);
-                        if (raycastHit.transform.gameObject == border)
-                        {
-                            Debug.Log("HIT!");
-                        }
+                        textObjectUI.SetActive(true);
                     }
                 }
             }
+
         }
 
-        // void LateUpdate()
-        // {
-        //     transform.forward = Camera.main.transform.forward;
-        //     Vector3 rotation = transform.rotation.eulerAngles;
-        //     rotation.x = 0;
-        //     transform.rotation = Quaternion.Euler(rotation);
-        // }
+        void Save()
+        {
+            // Upload
+            Debug.Log(inputField.text);
+            textObjectUI.SetActive(false);
+        }
+
+        void UpdateBoard(string data)
+        {
+            boardText.text = data;
+        }
+
+        void LateUpdate()
+        {
+            // transform.forward = arCamera.transform.forward;
+
+            Vector3 newPosition = transform.position;
+            newPosition.y = arCamera.transform.position.y;
+            transform.position = newPosition;
+
+            // Vector3 rotation = transform.rotation.eulerAngles;
+            // rotation.x = 0;
+            // transform.rotation = Quaternion.Euler(rotation);
+        }
     }
 }
